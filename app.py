a5204ed0b04c6e099e3471e8c7dac2334ce70ded
Ritika-02 <ritikabhandari049@gmail.com>
2024-03-19 13:18:44 +0530

    prediction

---
+++ app.py
@@ -0,0 +1,41 @@
+from src.WheatKernelClassification.pipelines.prediction_pipeline import CustomData,PredictPipeline
+from flask import Flask, request, render_template
+from src.WheatKernelClassification.logger import logging
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['GET','POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('form.html')
+    
+    else:
+        data = CustomData(
+
+            area= float(request.form.get('area')),
+            perimeter= float(request.form.get('perimeter')),
+            compactness= float(request.form.get('compactness')),
+            length_of_kernel= float(request.form.get('length_of_kernel')),
+            width_of_kernel= float(request.form.get('width_of_kernel')),
+            asymmetry_coefficient= float(request.form.get('asymmetry_coefficient')),
+            length_of_kernel_groove= float(request.form.get('length_of_kernel_groove'))
+        )
+
+        final_data = data.get_data_as_dataframe()
+        logging.info(f'{final_data}')
+
+        predict_pipeline = PredictPipeline()
+
+        pred = predict_pipeline.predict(final_data)
+
+        variety_labels = {3: 'Canadian', 2: 'Rosa', 1:'Kama'}
+        predicted_label = variety_labels[pred[0]]
+
+        return render_template('result.html', final_result = predicted_label)
+
+app.run()
+
--- src/WheatKernelClassification/pipelines/prediction_pipeline.py
+++ src/WheatKernelClassification/pipelines/prediction_pipeline.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 import pandas as pd
 from src.WheatKernelClassification.exception import customexception
 from src.WheatKernelClassification.logger import logging
@@ -24,4 +25,43 @@ class PredictPipeline:
             return pred
         
         except Exception as e:
+            raise customexception(e,sys)
+        
+class CustomData:
+    def __init__(self,
+                 area : float,
+                 perimeter : float,
+                 compactness : float,
+                 length_of_kernel : float,
+                 width_of_kernel : float,
+                 asymmetry_coefficient : float,
+                 length_of_kernel_groove : float
+                 ):
+        
+        self.area = area
+        self.perimeter = perimeter
+        self.compactness = compactness
+        self.length_of_kernel = length_of_kernel
+        self.width_of_kernel = width_of_kernel
+        self.asymmetry_coefficient = asymmetry_coefficient
+        self.length_of_kernel_groove = length_of_kernel_groove
+
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'area' : [self.area],
+                'perimeter' : [self.perimeter],
+                'compactness' : [self.compactness],
+                'length_of_kernel' : [self.length_of_kernel],
+                'width_of_kernel' : [self.width_of_kernel],
+                'asymmetry_coefficient': [self.asymmetry_coefficient],
+                'length_of_kernel_groove' : [self.length_of_kernel_groove],
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('DataFrame Gathered')
+            return df
+        
+        except Exception as e:
+            logging.info('Exception Occured in Prediction Pipeline')
             raise customexception(e,sys)
